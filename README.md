5bd07b991112176a4db492f922ce4fa4a111b2bb
Álvaro Arranz <aarranz@fi.upm.es>
2015-09-30 16:33:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Wirecloud-markdown-github
+=========================
+
+This python package recopiles some markdown extensions to make 
+[Python Markdown](https://pythonhosted.org/Markdown/) behave more like GFM markdown.
+This package not intent to build a full compatible parser of the GFM and its
+used mainly in the generation of the WireCloud's documentation as a temporal
+work around, our plan is to work for using the official code.
+
+Summary:
+
+- headerid: Modified from the headerid extension provided by the official [Python Markdown](https://pythonhosted.org/Markdown/)
+- superfences: Borrowed from [PyMdown Extensions](https://github.com/facelessuser/pymdown-extensions)
+- magiclink: Borrowed from [PyMdown Extensions](https://github.com/facelessuser/pymdown-extensions)
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='wirecloud-markdown-github',
+    version='1.0',
+    author='CoNWeT Lab',
+    author_email='wirecloud@conwet.com',
+    description='Set of markdown extensions for getting similar behaviour to GFM.',
+    url='https://github.com/conwetlab/markdown_github',
+    packages=['markdown_github'],
+    install_requires=['markdown>=2.0',],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Markup :: HTML'
+    ]
+)
